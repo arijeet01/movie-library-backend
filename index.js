d5e8fb50881cb42c85ff250be679daacfa18091d
arijeet01 <arijeetnayak2912@gmail.com>
2022-07-03 05:02:20 +0530

    lists done

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express=require("express");
 const cors=require("cors");
 const mongoose=require("mongoose");
-const bcrypt=require("bcrypt");
+const session = require("express-session");
+const passport= require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+app.use(session({
+    secret: "a9r8i7j6e5e4t3",
+    resave: false,
+    saveUninitialized: false
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/movieDB",{
     useNewUrlParser: true,
@@ -19,55 +29,121 @@ function(){
 
 const userSchema= new mongoose.Schema({
     name: String,
-    email: String,
-    password: String
+    username: String,
+    password: String,
 })
 
-const User = new mongoose.model("User", userSchema);
+const listSchema = new mongoose.Schema({
+    userId: String,
+    listname: String,
+    privacy: String,
+    movielist: [],
+    URL: String
+})
 
-app.post("/login", function(req, res){
-    const { email, password} = req.body; 
-    User.findOne({email: email}, async (err, user) =>{
-        if(user){
-            const compPass = await bcrypt.compare(password, user.password);
-            if(compPass){
-                res.send({message: "Login Successfull", user: user});
-            }else{
-                res.send({message: "Incorrect Credentials!"});
-            }
-        }else{
-            res.send({message: "Incorrect Credentials!"});
-        }
-    })
+userSchema.plugin(passportLocalMongoose, {
+    selectFields: 'name email'
 });
 
-app.post("/register",async function(req, res){
-    const {name, email, password} = req.body;
+const User = new mongoose.model("User", userSchema);
+const List = new mongoose.model("List", listSchema);
 
-    const salt = await bcrypt.genSalt(10); 
-    const secPassword= await bcrypt.hash(password, salt);
+passport.use(User.createStrategy());
 
-    
-    User.findOne({email: email}, (err, user) => {
-        if(user){
-            res.send({message: "User already registered!"});
-        }else{
-            const user = new User({
-                name,
-                email,
-                password: secPassword
-            })
-            user.save( err => {
-                if(err){
-                    res.send(err)
-                }else{
-                    res.send( {message: "Successfully Registered! Please Login now."});
-                }
-            })
-        }
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+app.get("/list/public/{listname}", function(){
+        //get movielist acc to the list name also there will be a condition which will check whether the list 
+        //is public or not
+});
+
+
+app.post("/login", function(req, res){
+    const { username, password} = req.body; 
+
+    const user = new User({
+        username: username,
+        password: password
     })
 
+    req.login(user, function(err){
+        if(err){
+            res.send({message: "Incorrect Credentials!"});
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                User.findOne({username: username},(err, user) => {
+                    if(req.isAuthenticated)
+                    res.send({message: "Login Successfull", user: user});
+                    else
+                    res.send({message: "Incorrect Credentials!"});
+                })
+               
+            });
+        }
+    });
+});
+
+app.post("/register", function(req, res){
+    const {name, username, password} = req.body;
+    User.register(new User({name, username}), password, function(err, user){
+        if(err){
+            res.send({message: "Invalid Input, please try again"});
+            console.log(err);
+        }else{
+            res.send( {message: "Successfully Registered! Please Login now."});
+        }
+    });
     
+});
+
+app.post("/createlist", function(req, res){
+    const {userId, listname, privacy, movielist}=req.body;
+    const newlist = new List({
+                    userId: userId,
+                    listname: listname,
+                    privacy: privacy,
+                    movielist: movielist,
+                    URL: 'abc'
+                })
+                List.findOne({'listname' :listname}, function(err, list){
+                    if(!err){
+                        if(!list){
+                            newlist.save(function(err,result){
+                                if (err){
+                                    console.log(err);
+                                }
+                                else{
+                                    console.log(result);
+                                    
+                                }
+                            });
+                        }else{
+                            res.send({message: "List already exists"});
+                        }
+                    }
+                })
+                
+            }
+);
+
+app.post("/list", function(req, res){
+    List.find(function(err, lists){
+        res.send(lists);
+    });
+});
+
+app.post("/addexisting", function(req, res){
+    const {list}=req.body;
+    const {movie}=req.body;
+    console.log(list.listname); 
+    console.log(movie);
+    List.findOne({'listname': list.listname}, function(err, list){
+        list.movielist.push(movie);
+        list.save();
+    });
 });
 
 app.listen(9002, function(){
